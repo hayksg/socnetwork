a77b23ecae6fc9f691c848cf4fd0d0bf319296b0
php-user <hayksg@gmail.com>
2017-10-08 09:04:36 -0400

    Last commit

--- module/ContactUs/src/Entity/ContactUs.php
+++ module/ContactUs/src/Entity/ContactUs.php
@@ -15,7 +15,8 @@ class ContactUs
      * @Annotation\Attributes({"class":"form-control", "id":"name", "required":"required"})
      * @Annotation\Options({"label":"Name", "label_attributes":{"class":"control-label col-sm-3"}, "min":"1", "max":"50"})
      * @Annotation\Required({"required":"true"})
-     * @Annotation\Filter({"name":"stripTags", "name":"stringTrim"})
+     * @Annotation\Filter({"name":"stripTags"})
+     * @Annotation\Filter({"name":"stringTrim"})
      * @Annotation\Validator({"name":"stringLength", "options":{"encoding":"utf-8", "min":"1", "max":"50"}})
      */
     private $name;
@@ -25,7 +26,8 @@ class ContactUs
      * @Annotation\Attributes({"class":"form-control", "id":"email", "required":"required"})
      * @Annotation\Options({"label":"Email", "label_attributes":{"class":"control-label col-sm-3"}})
      * @Annotation\Required({"required":"true"})
-     * @Annotation\Filter({"name":"stripTags", "name":"stringTrim"})
+     * @Annotation\Filter({"name":"stripTags"})
+     * @Annotation\Filter({"name":"stringTrim"})
      * @Annotation\Validator({"name":"emailAddress"})
      */
     private $email;
@@ -35,7 +37,8 @@ class ContactUs
      * @Annotation\Attributes({"class":"form-control", "id":"message", "required":"required"})
      * @Annotation\Options({"label":"Message", "label_attributes":{"class":"control-label col-sm-3"}, "min":"2", "max":"500"})
      * @Annotation\Required({"required":"true"})
-     * @Annotation\Filter({"name":"stripTags", "name":"stringTrim"})
+     * @Annotation\Filter({"name":"stripTags"})
+     * @Annotation\Filter({"name":"stringTrim"})
      * @Annotation\Validator({"name":"stringLength", "options":{"encoding":"utf-8", "min":"2", "max":"500"}})
      */
     private $message;
